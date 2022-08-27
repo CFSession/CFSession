adb5742d31e9b5eace5f6fab88df28f6d6ff731a
Kinuseka <66762783+Kinuseka@users.noreply.github.com>
2022-08-28 04:47:01 +0800

    Woopsie

--- CFSession/cfdirmodel.py
+++ CFSession/cfdirmodel.py
@@ -4,16 +4,6 @@ class cfDirectory:
     """cfDirectory object
     Handles directories where session tokens, headers, and cookie 
     dumps on CFSession.cfcookie.cfCookieHandler
-    
-    Basic Usage::
-      >>> frp, 
-      >>> s = CFSession.cfSession()
-      >>> s.get('https://httpbin.org/get')
-      <Response [200]>
-    Or as a context manager::
-      >>> with CFSession.cfSession() as s:
-      ...     s.get('https://httpbin.org/get')
-      <Response [200]>
     """
     def __init__(self, path: str = DEFAULT, cache_path: str = DEFAULT) -> None:
         self.path = path
