a1b51884b8966cf7e0d70803682c4c501012dbe4
Kinuseka <realkingseeker1089@gmail.com>
2023-01-18 18:50:08 +0800

    Identify if .exe is required

--- test/changeDirectory_test.py
+++ test/changeDirectory_test.py
@@ -2,6 +2,6 @@ from CFSession import cfDirectory
 from CFSession import cfSession
 import os
 if __name__ == "__main__":
-    session = cfSession(directory=cfDirectory("customdirectory",chromedriver_path=os.path.join(os.path.dirname(__file__), "bin/chromedriver108")))
+    session = cfSession(directory=cfDirectory("customdirectory",chromedriver_path=os.path.join(os.path.dirname(__file__), "bin/chromedriver108.exe")))
     res = session.get("https://nowsecure.nl") #A Cloudflare protected site
     print(res.content)
--- test/raise_test.py
+++ test/raise_test.py
@@ -3,6 +3,6 @@ from CFSession import cfDirectory
 import os
 
 if __name__ == "__main__":
-    session = cfSession(cfDirectory(chromedriver_path=os.path.join(os.path.dirname(__file__), "bin/chromedriver108")))
+    session = cfSession(cfDirectory(chromedriver_path=os.path.join(os.path.dirname(__file__), "bin/chromedriver108.exe")))
     res = session.post("https://nowsecure.nl") #Method Not Allowed
     res.raise_for_status()
