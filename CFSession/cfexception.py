7dd1da0177658d102f6d7450e705c197ebe9d1bf
Kinuseka <realkingseeker1089@gmail.com>
2023-04-21 19:52:57 +0800

    improved exceptions

--- CFSession/cfexception.py
+++ CFSession/cfexception.py
@@ -4,7 +4,7 @@ CFSession.cfexception
 This module contains the exceptions for CFSession
 """
 
-class CFException(Exception):
+class CFException(Exception): #Parent class
     def __init__(self,message=None,response=None,request=None, *args, **kwargs):
         """There was an ambiguous exception that occurred while handling your
     request."""
@@ -13,17 +13,17 @@ class CFException(Exception):
 
 
 
-class NetworkError(CFException):
+class NetworkError(CFException): #Main class
     def __init__(self, response=None,*args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
-class HTTPError(CFException):
+class HTTPError(CFException): #Main class
     def __init__(self, response=None, *args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
-class NotFound(HTTPError): 
+class NotFound(HTTPError): #Subclass
     #subclass
     def __init__(self, response=None,*args, **kwargs):
         # if any arguments are passed...
@@ -31,7 +31,7 @@ class NotFound(HTTPError):
         # maybe you will need to check kwargs here
         super().__init__(response=response, *args, **kwargs)
 
-class CloudflareBlocked(HTTPError):
+class CloudflareBlocked(HTTPError): #Subclass
     #subclass
     def __init__(self, response=None, *args, **kwargs):
         # if any arguments are passed...
@@ -39,33 +39,32 @@ class CloudflareBlocked(HTTPError):
         # maybe you will need to check kwargs here
         super().__init__(response=response, *args, **kwargs)
 
-class URLRequired(CFException):
+class URLRequired(CFException): #Main class
     def __init__(self, response, *args, **kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
-class TooManyRedirects(CFException):
+class TooManyRedirects(CFException): #Main class
     def __init__(self, response, *args, **kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
-class Timeout(CFException):
-    """The request timed out.
-
-    Catching this error will catch both
-    :exc:`~requests.exceptions.ConnectTimeout` and
-    :exc:`~requests.exceptions.ReadTimeout` errors.
-    """
-
-    pass
-
-class ConnectTimeout(Timeout):
-    def __init__(self, response, *args, **kwargs):
+class Timeout(CFException): #Main class
+    def __init__(self, response=None, *args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
-class ReadTimeout(Timeout):
-    def __init__(self, response, *args, **kwargs):
-        default_message = repr(response)
-        super().__init__(default_message,response=response, *args, **kwargs)
+class ConnectTimeout(Timeout): #Subclass
+    def __init__(self, response=None,*args, **kwargs):
+        # if any arguments are passed...
+        # If you inherit from the exception that takes message as a keyword
+        # maybe you will need to check kwargs here
+        super().__init__(response=response, *args, **kwargs)
+
+class ReadTimeout(Timeout): #Subclass
+    def __init__(self, response=None,*args, **kwargs):
+        # if any arguments are passed...
+        # If you inherit from the exception that takes message as a keyword
+        # maybe you will need to check kwargs here
+        super().__init__(response=response, *args, **kwargs)
 
