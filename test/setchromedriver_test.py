816b807dd6df0889e0b9eb6edaacbd48706263d8
Kinuseka <realkingseeker1089@gmail.com>
2023-04-21 19:53:55 +0800

    removed flawed test

--- test/setchromedriver_test.py
+++
@@ -1,7 +0,0 @@
-from CFSession import cfDirectory
-from CFSession import cfSession
-
-if __name__ == "__main__":
-    session = cfSession()
-    res = session.get("https://nowsecure.nl") #A Cloudflare protected site
-    print(res.content)
