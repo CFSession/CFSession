f2d6edaad23203191a9432710925065c2a0fe141
Kinuseka <realkingseeker1089@gmail.com>
2023-04-21 19:51:56 +0800

    test flawed fix

--- test/changeDirectory_test.py
+++ test/changeDirectory_test.py
@@ -2,6 +2,6 @@ from CFSession import cfDirectory
 from CFSession import cfSession
 import os
 if __name__ == "__main__":
-    session = cfSession()
+    session = cfSession(directory=cfDirectory('./artificialDirectory'))
     res = session.get("https://nowsecure.nl") #A Cloudflare protected site
     print(res.content)
