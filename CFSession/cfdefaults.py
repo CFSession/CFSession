8288c7bbe2523df6efe8e66debd511037872bb7a
Kinuseka <realkingseeker1089@gmail.com>
2023-08-06 20:04:49 +0800

    add constant class

--- CFSession/cfdefaults.py
+++ CFSession/cfdefaults.py
@@ -47,3 +47,6 @@ class Required_defaults:
     def desired_capabilites_default(self) -> DesiredCapabilities:
         self.dcp["pageLoadStrategy"] = "eager"
         return self.dcp
+
+class cfConstant:
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
