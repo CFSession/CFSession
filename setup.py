7649c8ae1662430e91296ffa8ccccb8775ec9ffb
Kinuseka <realkingseeker1089@gmail.com>
2023-08-10 05:53:34 +0800

    bump to 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'rt') as readme:
 
 setup(
     name='CFSession',
-    version='1.2.0',
+    version='1.2.1',
     author='Kinuseka',
     author_email='realkingseeker1089@gmail.com',
     description='A Cloudflare IUAM session grabber',
