94e6240a584139cecd6b02ba7ae18646891007b8
Kinuseka <realkingseeker1089@gmail.com>
2023-08-09 23:29:54 +0800

    merge cfConstant resolved

--- CFSession/cf.py
+++ CFSession/cf.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 #Modules
-from .cfdefaults import Required_defaults
+from .cfdefaults import Required_defaults, cfConstant
 from pathlib import Path
 import typing
 import time
