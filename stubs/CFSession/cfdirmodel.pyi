5ad08a495989730299276908e0f0ecc4b46b3e97
Kinuseka <realkingseeker1089@gmail.com>
2023-09-19 22:38:50 +0800

    add stubs

---
+++ stubs/CFSession/cfdirmodel.pyi
@@ -0,0 +1,17 @@
+from .cfdefaults import cfConstant as cfConstant
+from _typeshed import Incomplete
+from typing_extensions import Literal, Tuple
+
+DEFAULT: str
+DEFAULT_NAME: Tuple[Literal["Cookie Name"], Literal["User-Agent Name"]]
+
+class cfDirectory:
+    cache: str
+    cookie_name: str
+    agent_name: str
+    chromedriver: str
+    def __init__(self, cache_path: str = ..., session_name: Tuple[Literal["Cookie Name"], Literal["User-Agent Name"]]=DEFAULT_NAME, chromedriver_path: str = ...) -> None: ...
+    def cookie_path(self) -> str: ...
+    def session_path(self) -> str: ...
+    def cache_path(self) -> str: ...
+    def chromedriver_path(self) -> str: ...
