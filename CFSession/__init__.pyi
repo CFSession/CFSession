09988b251188decb5973aa3932bac1680204ce9f
Kinuseka <realkingseeker1089@gmail.com>
2023-10-14 04:14:49 +0800

    stubs

--- CFSession/__init__.pyi
+++ CFSession/__init__.pyi
@@ -1,6 +1,6 @@
-from . import CFBypass as CFBypass, SiteBrowserProcess as SiteBrowserProcess
-from . import cfSession as cfSession, cfSimulacrum as cfSimulacrum
-from . import cfCookieHandler as cfCookieHandler
-from . import Required_defaults as Required_defaults
-from . import cfDirectory as cfDirectory, Options as Options
-from . import CloudflareBlocked as CloudflareBlocked, ConnectTimeout as ConnectTimeout, HTTPError as HTTPError, NetworkError as NetworkError, NotFound as NotFound, ReadTimeout as ReadTimeout, Timeout as Timeout, TooManyRedirects as TooManyRedirects, URLRequired as URLRequired
+from .cf import CFBypass as CFBypass, SiteBrowserProcess as SiteBrowserProcess
+from .cfbrowser import cfSession as cfSession, cfSimulacrum as cfSimulacrum
+from .cfcookie import cfCookieHandler as cfCookieHandler
+from .cfdefaults import Required_defaults as Required_defaults
+from .cfmodels import cfDirectory as cfDirectory, Options as Options
+from .cfexception import CloudflareBlocked as CloudflareBlocked, ConnectTimeout as ConnectTimeout, HTTPError as HTTPError, NetworkError as NetworkError, NotFound as NotFound, ReadTimeout as ReadTimeout, Timeout as Timeout, TooManyRedirects as TooManyRedirects, URLRequired as URLRequired
