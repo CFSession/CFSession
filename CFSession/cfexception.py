93a87cfb8a4b1511019a005b0ae3b5087162d6fd
Kinuseka <realkingseeker1089@gmail.com>
2023-12-24 04:04:47 +0800

    Add proxy errors

--- CFSession/cfexception.py
+++ CFSession/cfexception.py
@@ -15,17 +15,19 @@ class CFException(Exception): #Parent class
 
 
 class NetworkError(CFException): #Main class
+    "Network error had occured"
     def __init__(self, response=None,*args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
 class HTTPError(CFException): #Main class
+    "HTTP related error had occured"
     def __init__(self, response=None, *args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
 class NotFound(HTTPError): #Subclass
-    #subclass
+    "HTTP-404 Not found error had occured"
     def __init__(self, response=None,*args, **kwargs):
         # if any arguments are passed...
         # If you inherit from the exception that takes message as a keyword
@@ -33,7 +35,7 @@ class NotFound(HTTPError): #Subclass
         super().__init__(response=response, *args, **kwargs)
 
 class CloudflareBlocked(HTTPError): #Subclass
-    #subclass
+    "Blocked by Cloudflare"
     def __init__(self, response=None, *args, **kwargs):
         # if any arguments are passed...
         # If you inherit from the exception that takes message as a keyword
@@ -41,21 +43,25 @@ class CloudflareBlocked(HTTPError): #Subclass
         super().__init__(response=response, *args, **kwargs)
 
 class URLRequired(CFException): #Main class
+    "URL Required error"
     def __init__(self, response, *args, **kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
 class TooManyRedirects(CFException): #Main class
+    "Too Many Redirects error"
     def __init__(self, response, *args, **kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
 class Timeout(CFException): #Main class
+    "Timeout Error"
     def __init__(self, response=None, *args,**kwargs):
         default_message = repr(response)
         super().__init__(default_message,response=response, *args, **kwargs)
 
 class ConnectTimeout(Timeout): #Subclass
+    "Connection Timeout error"
     def __init__(self, response=None,*args, **kwargs):
         # if any arguments are passed...
         # If you inherit from the exception that takes message as a keyword
@@ -63,9 +69,24 @@ class ConnectTimeout(Timeout): #Subclass
         super().__init__(response=response, *args, **kwargs)
 
 class ReadTimeout(Timeout): #Subclass
+    "Read Timeout error"
     def __init__(self, response=None,*args, **kwargs):
         # if any arguments are passed...
         # If you inherit from the exception that takes message as a keyword
         # maybe you will need to check kwargs here
         super().__init__(response=response, *args, **kwargs)
 
+class ProxyError(CFException): #Main class:
+    "Proxy related errors"
+    def __init__(self, message=None, response=None, request=None, *args, **kwargs):
+        super().__init__(message, response, request, *args, **kwargs)
+
+class ProxyConfigurationError(ProxyError): #Sub class:
+    "Proxy Configuration error"
+    def __init__(self, message=None, response=None, request=None, *args, **kwargs):
+        super().__init__(message, response, request, *args, **kwargs)
+
+class ProxyDecodeError(ProxyError): #Sub class:
+    "Proxy Decode error"
+    def __init__(self, message=None, response=None, request=None, *args, **kwargs):
+        super().__init__(message, response, request, *args, **kwargs)
